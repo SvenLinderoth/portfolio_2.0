fcfaefff29aee1800f7eadcae358c8fe036e55c7
Sven Linderoth <sl224hz@student.lnu.se>
2023-09-18 17:02:38 +0200

    -m fixed footer

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ function init () {
 
     explore_btn[0].addEventListener("click", scrollDown);
 
-    var scroll_up = document.getElementsByClassName('back-up-btn');
+    // var scroll_up = document.getElementsByClassName('back-up-btn');
 
-    scroll_up[0].addEventListener("click", scrollUp);
+    // scroll_up[0].addEventListener("click", scrollUp);
+
+    notiCard();
 }
 window.addEventListener('load', init);
 
@@ -19,12 +21,50 @@ function scrollDown(e) {
       });
     return null;
 }
-// void 
-function scrollUp(e) {
-    window.scrollTo({
-        top:  0,
-        left: 0,
-        behavior: "smooth",
-      });
-    return null;
+// // void 
+// function scrollUp(e) {
+//     window.scrollTo({
+//         top:  0,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//     return null;
+// }
+function notiCard() {
+  const el = document.querySelector(".noti")
+  const glowEl = document.querySelector(".notiglow")
+  const borderEl = document.querySelector(".notiborderglow")
+
+// if the pen is in thumbnail view, scale it up
+if (location.pathname.match(/fullcpgrid/i) ? true : false) {
+  document.documentElement.style.fontSize = "48px"
+  document.querySelector(".note").style.display = "none"
+}
+let isHovering = false;
+el.addEventListener("mousemove", (event) => {
+  const rect = el.getBoundingClientRect()
+  const localX = (event.clientX - rect.left) / rect.width
+  const localY = (event.clientY - rect.top) / rect.height
+  
+  console.log(localX, localY)
+  glowEl.style.left = localX * 100 + "%"
+  glowEl.style.top = localY * 100 + "%"
+  
+  borderEl.style.left = localX * 100 + "%"
+  borderEl.style.top = localY * 100 + "%"
+  
+  if (isHovering) {
+    glowEl.style.transition = "inset 50ms linear, opacity 300ms ease";
+    borderEl.style.transition = "inset 50ms linear, opacity 300ms ease"
+  } else {
+    glowEl.style.transition = "opacity 300ms ease"
+    borderEl.style.transition = "opacity 300ms ease"
+  }
+  
+  isHovering = false
+});
+
+el.addEventListener("mouseout", (event) => {
+  isHovering = true;
+})
 }
